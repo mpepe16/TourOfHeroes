65d83b9e4287ae871e84401f05ecb55964bf88a6
mpepe16 <mpepe66@gmail.com>
2022-09-17 21:20:00 +0200

    Add: addHero method.

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -66,6 +66,14 @@ updateHero(hero: Hero): Observable<any> {
     catchError(this.handleError<any>('updateHero'))
   );
 }
+
+/** POST: add a new hero to the server */
+addHero(hero: Hero): Observable<Hero> {
+  return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+    catchError(this.handleError<Hero>('addHero'))
+  );
+}
   
   constructor(
     private messageService: MessageService,
