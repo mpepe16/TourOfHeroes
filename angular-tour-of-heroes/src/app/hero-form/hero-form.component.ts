8f05173dda44df08245492f1cea2390e5d22f6ca
mpepe16 <mpepe66@gmail.com>
2022-09-28 11:17:19 +0200

    Add: dummy datas and onSubmit function to the hero-form component.

--- angular-tour-of-heroes/src/app/hero-form/hero-form.component.ts
+++ angular-tour-of-heroes/src/app/hero-form/hero-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroClass } from '../heroClass';
 
 @Component({
   selector: 'app-hero-form',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-form.component.css']
 })
 export class HeroFormComponent implements OnInit {
-
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+  model = new HeroClass(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  submitted = false;
+  
+  onSubmit() { this.submitted = true; }
   constructor() { }
 
   ngOnInit(): void {
+  }
+  skyDog(): HeroClass {
+    const myHero =  new HeroClass(42, 'SkyDog',
+                           'Fetch any object at any distance',
+                           'Leslie Rollover');
+    console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
+    return myHero;
   }
 
 }
